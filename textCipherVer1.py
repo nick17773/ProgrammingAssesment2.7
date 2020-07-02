bfd445adef1a3e2c5241f76c5214dfe8784b8e51
nick17773 <n.a.russell2015@gmail.com>
2020-07-03 10:42:13 +1200

    version 4.041
    updated the text .exe file and the .py file to include a new start message.

--- textCipherVer1.py
+++ textCipherVer1.py
@@ -64,7 +64,7 @@ def cipher(message_history, key_history):
         return translated_message
 
 
-startMsg = "Hi, welcome to my cipher tool, Please input yes to continue, and input a message and a keyword as a key, then select either encrypt or decrypt to run the program. " \
+startMsg = "Hi, welcome to my cipher tool, Please input yes to continue. \nThen input a message and a keyword as a key, then select either encrypt or decrypt to run the program. " \
            "\nOtherwise please input no. Information inputted will be stored in both a csv and txt file when the program ends."
 yes = ["Yes", "yes", "y", "Y"]  # list for possible clauses to be used to continue the loop relating to yes
 no = ["No", "no", "n", "N"]  # list for possible clauses to be used to end the loop relating to no
