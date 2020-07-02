24f81330a55ee1a94d9993f91488643f9159f900
nick17773 <n.a.russell2015@gmail.com>
2020-07-03 09:28:39 +1200

    version 4.03
    combined the original loader with the new clear function, so both are usable from the same file. .exe file to come soon.

--- historyLoad.py
+++ historyLoad.py
@@ -23,5 +23,23 @@ while loop:
         print("Values appended to output.txt")
     elif fileType == "end":
         loop = False
+
+    elif fileType == "clear":
+        loop2 = True
+        while loop2:
+            toClearOrNotToClear = input("Do you want to clear the files? ")
+            txtfile = "cipherHistory.txt"
+            csvfile = "cipherHistory.csv"
+            if toClearOrNotToClear == "yes":
+                with open(txtfile, "w") as output:  # with statement that outputs the history to a text file
+                    writer = csv.writer(output, lineterminator='\n', dialect='excel')
+                    writer.writerows("")
+                with open(csvfile, "w") as output:  # with statement that outputs the history to a text file
+                    writer = csv.writer(output, lineterminator='\n', dialect='excel')
+                    writer.writerows("")
+                print("Files have been cleared")
+            else:
+                print("Data is still stored, goodbye")
+                loop2 = False
     else:
-        print("Please enter either text, csv or end to continue")
+        print("Please enter either text, csv, clear or end to continue")
